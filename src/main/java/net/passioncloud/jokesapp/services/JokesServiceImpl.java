05d4f3c7b8ecc3ab65aaeb539d50caf57860b600
Gilbert <gssenyonjo@gmail.com>
2022-08-21 14:13:23 +0300

    looked at configuring beans using @Configuration

--- src/main/java/net/passioncloud/jokesapp/services/JokesServiceImpl.java
+++ src/main/java/net/passioncloud/jokesapp/services/JokesServiceImpl.java
@@ -1,15 +1,18 @@
 package net.passioncloud.jokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokesServiceImpl implements JokesService {
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokesServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    @Autowired
+    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
+
 
     @Override
     public String getJoke() {
